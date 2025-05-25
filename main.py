fde8ae3a0d35da1414654e85db9973c7aef8b410
FelipeTorresP <pipe1896@gmail.com>
2025-05-24 19:40:59 -0500

    Se modifico mensaje para que sea mas diciente para el cliente

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def obtener_digitos_pi(cantidad):
     return pi_str[:cantidad+2]  # Se suma 2 para incluir "3."
 
 if __name__ == '__main__':
-    cantidad_digitos = int(input("Ingrese la cantidad de dígitos de π que desea ver: "))
+    cantidad_digitos = int(input("Ingrese la cantidad de numeros de π que desea ver: "))
     print(f'Los primeros {cantidad_digitos} dígitos de π son: {obtener_digitos_pi(cantidad_digitos)}')
