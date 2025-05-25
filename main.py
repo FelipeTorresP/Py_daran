41730f19aedfc3b14bea673c094318ab5148a8b1
FelipeTorresP <49736897+FelipeTorresP@users.noreply.github.com>
2025-05-24 19:44:47 -0500

    Update main.py
    Se cambio pi por euler

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import math
 
-def obtener_digitos_pi(cantidad):
-    pi_str = str(math.pi)  # Convertir π a cadena
-    return pi_str[:cantidad+2]  # Se suma 2 para incluir "3."
+def obtener_digitos_e(cantidad):
+    e_str = str(math.e)  # Convertir e a cadena
+    return e_str[:cantidad+2]  # Se suma 2 para incluir "2."
 
 if __name__ == '__main__':
-    cantidad_digitos = int(input("Ingrese la cantidad de numeros de π que desea ver: "))
-    print(f'Los primeros {cantidad_digitos} dígitos de π son: {obtener_digitos_pi(cantidad_digitos)}')
+    cantidad_digitos = int(input("Ingrese la cantidad de dígitos de e que desea ver: "))
+    print(f'Los primeros {cantidad_digitos} dígitos de e son: {obtener_digitos_e(cantidad_digitos)}')
