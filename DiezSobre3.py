f3e7086a0c242558e20ff7dbc49a6e88a8126e61
FelipeTorresP <pipe1896@gmail.com>
2025-05-24 20:03:03 -0500

    Se agrego funcionalidad ditos de 10 sobre 3

---
+++ DiezSobre3.py
@@ -0,0 +1,7 @@
+def obtener_digitos_division(cantidad):
+    resultado = str(10 / 3)  # Convertir el resultado de la división a cadena
+    return resultado[:cantidad+2]  # Se suma 2 para incluir "3."
+
+if __name__ == '__main__':
+    cantidad_digitos = int(input("Ingrese la cantidad de dígitos de 10-3 que desea ver: "))
+    print(f'Los primeros {cantidad_digitos} dígitos de 10/3 son: {obtener_digitos_division(cantidad_digitos)}')
